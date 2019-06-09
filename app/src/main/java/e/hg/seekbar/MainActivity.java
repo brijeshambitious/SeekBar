30148723f19ac65bcc3bf2b1edceb84d2b6c099b
brijeshambitious <hitesh56103@gmail.com>
2019-06-09 18:59:13 +0530

    Initial commit

---
+++ app/src/main/java/e/hg/seekbar/MainActivity.java
@@ -0,0 +1,50 @@
+package e.hg.seekbar;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private SeekBar seekBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView=findViewById(R.id.textView);
+        seekBar=findViewById(R.id.seekBar);
+        textView.setText("Scale: "+seekBar.getProgress()+"/"+seekBar.getMax());
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                textView.setTextColor(Color.BLACK);
+                textView.setText("Scale: "+seekBar.getProgress()+"/"+seekBar.getMax());
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                if (seekBar.getProgress()>=7){
+                    textView.setTextColor(Color.RED);
+                }
+
+            }
+        });
+
+    }
+
+
+
+
+
+}
